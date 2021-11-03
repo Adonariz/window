de3e667f4dcfa224332d85279a9d546ed4b87b03
A Zhila <aleksej.zhila@gmail.com>
2021-11-03 18:01:24 +0300

    добавляет работу с формами (отправка + валидация)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
+// const dist = "E:/Install/Software/openserver/full 5.3.7/OpenServer/domains/window";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
